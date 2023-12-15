e5e01d8edadcbe1ca9c140a3d5958fa9656264b4
Michael Vogel <michael.vogel@flagbit.de>
2023-12-15 11:52:12 +0100

    feat: Add additional values to question and an interface

--- src/Questions/Question.php
+++ src/Questions/Question.php
@@ -2,21 +2,27 @@
 
 namespace Vogaeael\MultipleChoiceQuestionConsole\Questions;
 
-class Question
+class Question implements QuestionInterface
 {
     private string $question;
     /** @var string[] $wrongAnswers */
     private array $wrongAnswers;
     private string $rightAnswer;
+    private int $wrongAnswered;
+    private int $rightAnswered;
 
     public function __construct(
         string $question,
         array $wrongAnswers,
-        string $rightAnswer
+        string $rightAnswer,
+        int $wrongAnswered = 0,
+        int $rightAnswered = 0
     ) {
         $this->question = $question;
         $this->wrongAnswers = $wrongAnswers;
         $this->rightAnswer = $rightAnswer;
+        $this->wrongAnswered = $wrongAnswered;
+        $this->rightAnswered = $rightAnswered;
     }
 
     /**
@@ -41,5 +47,29 @@ class Question
     public function getRightAnswer(): string
     {
         return $this->rightAnswer;
+    }
+
+    public function increaseWrongAnswered(): void
+    {
+        $this->wrongAnswered++;
+    }
+
+    public function increaseRightAnswered(): void
+    {
+        $this->rightAnswered++;
+    }
+
+    public function getPercentCorrectAnswered(): float
+    {
+        if (0 === $this->rightAnswered) {
+            return 0;
+        }
+
+        return ($this->rightAnswered / ($this->rightAnswered + $this->wrongAnswered)) * 100;
+    }
+
+    public function howOftenAnswered(): int
+    {
+        return $this->rightAnswered + $this->wrongAnswered;
     }
 }
