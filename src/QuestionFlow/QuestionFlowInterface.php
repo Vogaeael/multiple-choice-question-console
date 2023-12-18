1be05459fe36aa2efc8b36c8ac74f5fd764ea9e8
Michael Vogel <michael.vogel@flagbit.de>
2023-12-18 08:41:21 +0100

    fix: Adjust parameter type of question flow run

--- src/QuestionFlow/QuestionFlowInterface.php
+++ src/QuestionFlow/QuestionFlowInterface.php
@@ -2,9 +2,9 @@
 
 namespace Vogaeael\MultipleChoiceQuestionConsole\QuestionFlow;
 
-use Vogaeael\MultipleChoiceQuestionConsole\Questions\QuestionCollection\QuestionCollection;
+use Vogaeael\MultipleChoiceQuestionConsole\Questions\QuestionCollection\QuestionCollectionInterface;
 
 interface QuestionFlowInterface
 {
-    public function run(QuestionCollection $questions): void;
+    public function run(QuestionCollectionInterface $questions): void;
 }
