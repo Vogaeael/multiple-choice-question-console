bad7e2edab808efd6dd766ed02f2d1c8aacae036
Michael Vogel <michael.vogel@flagbit.de>
2023-12-21 09:01:30 +0100

    refactor: Improve file question loader to divide the jobs

---
+++ src/QuestionLoader/FileContentLoader.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Vogaeael\MultipleChoiceQuestionConsole\QuestionLoader;
+
+use Exception;
+
+class FileContentLoader
+{
+    /**
+     * @throws Exception
+     */
+    public function load(string $path): string
+    {
+        $this->validateFileExistAndReadable($path);
+
+        return file_get_contents($path);
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function validateFileExistAndReadable(string $path): void
+    {
+        if (!file_exists($path)) {
+            throw new Exception(sprintf('File `%s` does not exist', $path));
+        }
+        if (!is_file($path)) {
+            throw new Exception(sprintf('`%s` is not a file', $path));
+        }
+        if (!is_readable($path)) {
+            throw new Exception(sprintf('File `%s` is not readable', $path));
+        }
+    }
+}
--- src/QuestionLoader/FileQuestionLoader.php
+++ src/QuestionLoader/FileQuestionLoader.php
@@ -5,25 +5,14 @@ namespace Vogaeael\MultipleChoiceQuestionConsole\QuestionLoader;
 
 use Exception;
 use Vogaeael\MultipleChoiceQuestionConsole\QuestionLoader\Normalizer\QuestionNormalizerInterface;
-use Vogaeael\MultipleChoiceQuestionConsole\Questions\QuestionFactory;
-use Vogaeael\MultipleChoiceQuestionConsole\Questions\QuestionCollection\QuestionCollectionFactory;
 use Vogaeael\MultipleChoiceQuestionConsole\Questions\QuestionCollection\QuestionCollectionInterface;
 
 class FileQuestionLoader implements QuestionLoaderInterface
 {
-    protected const QUESTION_KEY_QUESTION_TEXT = 'questionText';
-    protected const QUESTION_KEY_WRONG_ANSWERS = 'wrongAnswers';
-    protected const QUESTION_KEY_CORRECT_ANSWER = 'correctAnswer';
-    protected const REQUIRED_KEYS = [
-        self::QUESTION_KEY_QUESTION_TEXT,
-        self::QUESTION_KEY_WRONG_ANSWERS,
-        self::QUESTION_KEY_CORRECT_ANSWER
-    ];
-
     public function __construct(
-        protected readonly QuestionCollectionFactory $questionCollectionFactory,
-        protected readonly QuestionFactory $questionFactory,
-        protected readonly QuestionNormalizerInterface $normalizer
+        protected readonly FileContentLoader $fileContentLoader,
+        protected readonly QuestionNormalizerInterface $normalizer,
+        protected readonly ArrayToQuestionInQuestionCollectionTransformer $transformer
     ) {}
 
     /**
@@ -31,84 +20,9 @@ class FileQuestionLoader implements QuestionLoaderInterface
      */
     public function load(string $path, string $type): QuestionCollectionInterface
     {
-        $this->validateFileExistAndReadable($path);
-        $questionsArray = $this->getContent($path);
+        $content = $this->fileContentLoader->load($path);
+        $questionsArray = $this->normalizer->normalize($content);
 
-        return $this->transformToQuestions($questionsArray, $type);
-    }
-
-    /**
-     * @throws Exception
-     */
-    protected function validateFileExistAndReadable(string $path): void
-    {
-        if (!file_exists($path)) {
-            throw new Exception(sprintf('File `%s` does not exist', $path));
-        }
-        if (!is_file($path)) {
-            throw new Exception(sprintf('`%s` is not a file', $path));
-        }
-        if (!is_readable($path)) {
-            throw new Exception(sprintf('File `%s` is not readable', $path));
-        }
-    }
-
-    protected function getContent(string $path): array
-    {
-        $content = file_get_contents($path);
-
-        return $this->normalizer->normalize($content);
-    }
-
-    /**
-     * @throws Exception
-     */
-    protected function transformToQuestions(array $questionsArray, string $type): QuestionCollectionInterface {
-        if (!array_key_exists('questions', $questionsArray)) {
-            throw new Exception('input file has not entry `questions`');
-        }
-        $questionsArray = $questionsArray['questions'];
-        $questions = $this->questionCollectionFactory->create($type);
-
-        foreach ($questionsArray as $questionArray) {
-            try {
-                $this->validateArrayKeys($questionArray);
-                $wrongAnswers = $questionArray[self::QUESTION_KEY_WRONG_ANSWERS];
-                if (!is_array($wrongAnswers)) {
-                    $wrongAnswers = [$wrongAnswers];
-                }
-                $question = $this->questionFactory->create(
-                    $questionArray[self::QUESTION_KEY_QUESTION_TEXT],
-                    $wrongAnswers,
-                    $questionArray[self::QUESTION_KEY_CORRECT_ANSWER]
-                );
-                $questions->add($question);
-            } catch (Exception $e) {
-                echo $e->getMessage() . PHP_EOL;
-                continue;
-            }
-        }
-
-        return $questions;
-    }
-
-    /**
-     * @throws Exception
-     */
-    protected function validateArrayKeys(array $questionArray): void
-    {
-        foreach(self::REQUIRED_KEYS as $requiredKey) {
-            $this->validateArrayForKey($questionArray, $requiredKey);
-        }
-    }
-
-    /**
-     * @throws Exception
-     */
-    protected function validateArrayForKey(array $questionArray, string $key): void
-    {
-        if (!array_key_exists($key, $questionArray)) {
-            throw new Exception(sprintf('Array has not the key %s', $key));
-        }
+        return $this->transformer->transformToQuestions($questionsArray, $type);
     }
 }
