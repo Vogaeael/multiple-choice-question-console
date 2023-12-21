6aee0d74ba1808725768595db5c0f1fcd363b424
Michael Vogel <michael.vogel@flagbit.de>
2023-12-21 10:34:24 +0100

    test: Add unit test for file question loader

---
+++ tests/unit/QuestionLoader/FileLoader/FileQuestionLoaderTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace unit\QuestionLoader\FileLoader;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Vogaeael\MultipleChoiceQuestionConsole\QuestionLoader\ArrayToQuestionInQuestionCollectionTransformer;
+use Vogaeael\MultipleChoiceQuestionConsole\QuestionLoader\FileLoader\FileContentLoader;
+use Vogaeael\MultipleChoiceQuestionConsole\QuestionLoader\FileLoader\FileQuestionLoader;
+use Vogaeael\MultipleChoiceQuestionConsole\QuestionLoader\Normalizer\QuestionNormalizerInterface;
+use Vogaeael\MultipleChoiceQuestionConsole\Questions\QuestionCollection\QuestionCollectionInterface;
+
+class FileQuestionLoaderTest extends TestCase
+{
+    protected FileContentLoader & MockObject $fileContentLoader;
+    protected QuestionNormalizerInterface & MockObject $normalizer;
+    protected ArrayToQuestionInQuestionCollectionTransformer & MockObject $transformer;
+    protected QuestionCollectionInterface & MockObject $questionCollection;
+    protected FileQuestionLoader $fileQuestionLoader;
+
+    protected function setUp(): void
+    {
+        $this->fileContentLoader = $this->createMock(FileContentLoader::class);
+        $this->normalizer = $this->createMock(QuestionNormalizerInterface::class);
+        $this->transformer = $this->createMock(ArrayToQuestionInQuestionCollectionTransformer::class);
+        $this->questionCollection = $this->createMock(QuestionCollectionInterface::class);
+
+        $this->fileQuestionLoader = new FileQuestionLoader($this->fileContentLoader, $this->normalizer, $this->transformer);
+    }
+
+    public function testLoad(): void
+    {
+        $pathToFile = 'path/to/file';
+        $type = 'type';
+        $contentOfFile = 'content of file';
+        $questionsArray = [
+            'questions' => [
+                [
+                    'questionText' => 'Is this question one?',
+                    'wrongAnswers' => [
+                        'wrong answer a.',
+                        'wrong answer b.',
+                    ],
+                    'correctAnswer' => 'correct answer.',
+                ],
+                [
+                    'questionText' => 'Is this question two?',
+                    'wrongAnswers' => 'No!',
+                    'correctAnswer' => 'Yes!',
+                ],
+            ],
+        ];
+        $this->fileContentLoader->method('load')
+            ->with($pathToFile)
+            ->willReturn($contentOfFile);
+        $this->normalizer->method('normalize')
+            ->with($contentOfFile)
+            ->willReturn($questionsArray);
+        $this->transformer->method('transformToQuestions')
+            ->with($questionsArray, $type)
+            ->willReturn($this->questionCollection);
+
+        $actual = $this->fileQuestionLoader->load($pathToFile, $type);
+
+        $this->assertSame($this->questionCollection, $actual);
+    }
+}
